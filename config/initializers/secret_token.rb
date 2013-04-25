a36e063169970fa2f37e4c1f79606eb917bcffb8
Maciej Kocyła <maciej.kocyla@gmail.com>
2013-04-25 11:17:57 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkcutter::Application.config.secret_token = '622c7c31aa45fe33266ed6b77e0e6a52bcbf9a57f889ef10f3c7419851cd6567cc99cf91d1a21f8eae9755359b7b6f3d8fcc0a2e2c292db4e7079556912430ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Linkcutter::Application.config.session_store :cookie_store, key: '_linkcutter_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Linkcutter::Application.config.session_store :active_record_store
