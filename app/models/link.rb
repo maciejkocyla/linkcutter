c816630ce59dbb94e8c3a9cb684d59776f7cb4e2
Maciej Kocyła <maciej.kocyla@gmail.com>
2013-04-26 21:08:54 +0200

    better short link validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,15 +3,14 @@ class Link < ActiveRecord::Base
   attr_accessible :short_url, :full_url
 
   VALID_URL = /https?:\/\/[\S]+/ 
+  VALID_SHORT_URL =  /^[A-Za-z\d_]+$/
 
-  validates :short_url, presence: true, uniqueness: true
+  validates :short_url, presence: true, uniqueness: true 
+  validates :short_url, format: {with: VALID_SHORT_URL, :message => "Can only be alphanumeric"}
   validates :full_url, presence: true, format: { with: VALID_URL}
   validate :working_link
 
-  def rebuild_routes
-    Rails.application.reload_routes!
-  end
-
+ 
   def self.search(search)
     if search
       where('full_url LIKE ?', "%#{search}%")
@@ -25,7 +24,7 @@ class Link < ActiveRecord::Base
       url = self.full_url
       open(url)
     rescue 
-      errors.add(:full_url, "sorry pal, but #{url} doesn't work")    
+      errors.add(:full_url, "You typed or pasted leads to nowhere")    
     end
     
   end
