b164d1381a6ac763251e9c853887fa17246425ed
Maciej <maciej@maciej-VirtualBox.(none)>
2013-04-28 20:59:33 +0200

    validation bugfix

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,11 +1,11 @@
 class Link < ActiveRecord::Base
   require 'open-uri'
-  require 'net/http'
+  require 'socket'
   attr_accessible :short_url, :full_url
 
   VALID_URL = /https?:\/\/[\S]+/ 
   VALID_SHORT_URL =  /^[A-Za-z\d_]+$/
-  SELF_URL = /[https?]?localhost:3000[\/\w+\d*]?/
+  SELF_URL = /[https?]?linkcutter\.herokuapp\.com[\/\w+\d*]?/
 
   validates :short_url, format: {with: VALID_SHORT_URL, :message => "Can only be alphanumeric"}, uniqueness: true, :if => :validate_short_url 
   validates :full_url, presence: true, format: { with: VALID_URL}, uniqueness: true
